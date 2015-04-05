2c77b62085947556b10308eb4280deffcc98df3d
Nathan Bernard <natedawg1013@gmail.com>
2015-04-05 03:23:04 -0400

    Updated AudioInput with required static methods

--- LetThereBeLight/ltbl/AudioInput.java
+++ LetThereBeLight/ltbl/AudioInput.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.sound.sampled.*;
 
 public class AudioInput{
+	private RingBuffer rb;
 
 	public static void getAvailableLines(){
 		Mixer.Info[] mixers = AudioSystem.getMixerInfo();
@@ -28,9 +29,59 @@ public class AudioInput{
 			System.out.println("Available line: " + info);
 		}
 	}
+	
+	public AudioInput(String name, int samplerate, int bufferLength){
+		rb = new RingBuffer(bufferLength);
+	}
+	
+	public static List<Mixer.Info> getSoundCards(){
+		Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+		List<Mixer.Info> cards = new ArrayList<Mixer.Info>();
+		for (Mixer.Info mixerInfo : mixers){
+			if(mixerInfo.getName().contains("Port")){
+				cards.add(mixerInfo);
+			}
+		}
+		return cards;
+	}
+	
+	public static List<Line.Info> getSources(Mixer.Info card){
+		Mixer m = AudioSystem.getMixer(card);
+		Line.Info[] lines = m.getSourceLineInfo();
+		ArrayList<Line.Info> sources = new ArrayList<Line.Info>();
+		for (Line.Info li : lines){
+			sources.add(li);
+		}
+		return sources;
+	}
+	
+	public static List<Line.Info> getTargets(Mixer.Info card){
+		Mixer m = AudioSystem.getMixer(card);
+		Line.Info[] lines = m.getTargetLineInfo();
+		ArrayList<Line.Info> sources = new ArrayList<Line.Info>();
+		for (Line.Info li : lines){
+			sources.add(li);
+		}
+		return sources;
+	}
 
 	public static void main(String args[]){
-		getAvailableLines();
+		Mixer.Info port = getSoundCards().get(0);
+		for(Line.Info li : getSources(port)){
+			System.out.println(li.toString().substring(0, li.toString().length()-12));
+			try {
+				AudioSystem.getLine(li).open();
+				Control[] controls = AudioSystem.getLine(li).getControls();
+				AudioSystem.getLine(li).close();
+				for(Control c : controls){
+					System.out.println(" - " + c.getType());
+				}
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
+
 }
 
