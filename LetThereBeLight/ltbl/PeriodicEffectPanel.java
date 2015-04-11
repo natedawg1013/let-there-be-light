22afe3b44e670e96e9942058ff319228b7b938ab
Nathan Bernard <natedawg1013@gmail.com>
2015-04-11 17:38:26 -0400

    Added periodic effects window

---
+++ LetThereBeLight/ltbl/PeriodicEffectPanel.java
@@ -0,0 +1,80 @@
+package ltbl;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PeriodicEffectPanel extends JPanel implements ActionListener{
+	JLabel temp;
+	JLabel onOff;
+	JCheckBox on;
+	JLabel sync;
+	JCheckBox beatmatch;
+	JLabel text;
+	JTextField one;
+	JLabel colon;
+	JTextField two;
+	public PeriodicEffectPanel(){
+		super(new GridBagLayout());
+		onOff = new JLabel("Enabled:");
+		on = new JCheckBox();
+		sync = new JLabel("Beatmatch:");
+		beatmatch = new JCheckBox();
+		text = new JLabel("BPM:");
+		one = new JTextField(5);
+		colon = new JLabel(":");
+		two = new JTextField(5);	
+		
+		colon.setVisible(false);
+		two.setVisible(false);
+		beatmatch.addActionListener(this);
+
+		placeComponents();
+	}
+	
+	private void placeComponents(){
+		GridBagConstraints c = new GridBagConstraints();
+			c.weightx=0.5; c.weighty=0.5;
+			c.anchor=GridBagConstraints.NORTH;
+			c.gridx=0; c.gridy=0;
+		this.add(onOff, c);
+			c.gridx=1;
+		this.add(on, c);
+			c.gridx=2;
+		this.add(sync);
+			c.anchor=GridBagConstraints.NORTH;
+			c.gridx=3;
+		this.add(beatmatch, c);
+			c.gridx=0; c.gridy=1;
+		this.add(text, c);
+			c.gridy=2;
+		this.add(one, c);
+			c.gridx=1;
+		this.add(colon, c);
+			c.gridx=2;
+		this.add(two, c);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==beatmatch){
+			boolean bm = beatmatch.isSelected();
+			if(bm)
+				text.setText("Ratio:");
+			else
+				text.setText("BPM");
+			
+			colon.setVisible(bm);
+			two.setVisible(bm);
+		}
+		
+	}
+}
--- LetThereBeLight/ltbl/PeriodicEffectWindow.java
+++ LetThereBeLight/ltbl/PeriodicEffectWindow.java
@@ -1,7 +1,55 @@
 package ltbl;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 
-public class PeriodicEffectWindow extends JPanel {
-    
+public class PeriodicEffectWindow extends JTabbedPane implements ActionListener {
+	AddButton button;
+	JPanel dummy;
+	
+	public PeriodicEffectWindow(){
+		super();
+		button = new AddButton();
+		dummy = new JPanel();
+		button.addActionListener(this);
+		this.addTab("1", dummy);
+		this.setTabComponentAt(0, button);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button.add){
+			this.remove(this.getTabCount()-1);
+			this.addTab(String.valueOf(this.getTabCount()+1), new PeriodicEffectPanel());
+			this.addTab("", dummy);
+			this.setTabComponentAt(this.getTabCount()-1, button);
+		}
+		
+	}
+	
+	class AddButton extends JPanel{
+		JButton add;
+		public AddButton(){
+			super(new GridLayout());
+			this.setOpaque(false);
+			add = new JButton("+");
+			add.setMargin(new Insets(0, 0, 0, 0));
+			add.setOpaque(false);
+			add.setContentAreaFilled(false);
+			add.setBorderPainted(false);
+			this.add(add);
+		}
+		
+		public void addActionListener(ActionListener l){
+			add.addActionListener(l);
+		}
+	}
+
 }
