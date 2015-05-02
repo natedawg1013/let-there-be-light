23bc6f9568e42e3ead8cab9d3a91c4e1c7d09083
Nathan Bernard <natedawg1013@gmail.com>
2015-05-01 22:36:16 -0400

    added serial DMX control

--- LetThereBeLight/ltbl/io/DMXOut.java
+++ LetThereBeLight/ltbl/io/DMXOut.java
@@ -1,16 +1,28 @@
 package ltbl.io;
 
+import gnu.io.*;
+
 public class DMXOut implements Output{
-    public DMXOut(String portbox, String dmxverbox) {
+	SerialComm comm;
+    
+	public DMXOut(CommPortIdentifier port, int baud) {
+		try {
+			comm = new SerialComm(port, baud);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 	@Override
 	public void setChannel(int channel, float value) {
-		// TODO Auto-generated method stub
-		
+		byte[] data = new byte[2];
+		data[0]=(byte) channel;
+		data[1]=(byte) (value*255);
+		comm.write(data, 0, 2);
 	}
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		// Unnecessary, controller will update on its own schedule
 	}
     
 }
