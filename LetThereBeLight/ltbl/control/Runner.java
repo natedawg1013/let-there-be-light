794fcbf36e470bd8692aa025091c306b4b1bfcfa
Nathan Bernard <natedawg1013@gmail.com>
2015-05-12 02:02:55 -0400

    updated all RXTX libraries to 2.2, enabled local lib folder library loading

--- LetThereBeLight/ltbl/control/Runner.java
+++ LetThereBeLight/ltbl/control/Runner.java
@@ -3,6 +3,10 @@ package ltbl.control;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,12 +36,40 @@ public class Runner implements ActionListener/*, Runnable*/{
 	private FourierAnalysis fourier;
 	private Thread outThread;
 	
-    public static void main(String[] args) throws LineUnavailableException {
+    public static void main(String[] args) throws LineUnavailableException, IOException, URISyntaxException {
+    	String loc = Runner.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+    	loc = loc.substring(0, loc.lastIndexOf('/')) + "/lib";
+    	addDir(loc);
     	Runner runner = new Runner();
     	//instantiate runner;
         //instantiate all classes
     	runner.showMainMenu(true);
     }
+    
+    public static void addDir(String s) throws IOException {
+		try {
+			// This enables the java.library.path to be modified at runtime
+			// From a Sun engineer at http://forums.sun.com/thread.jspa?threadID=707176
+			// 
+			Field field = ClassLoader.class.getDeclaredField("usr_paths");
+			field.setAccessible(true);
+			String[] paths = (String[])field.get(null);
+			for (int i = 0; i < paths.length; i++) {
+				if (s.equals(paths[i])) {
+					return;
+				}
+			}
+			String[] tmp = new String[paths.length+1];
+			System.arraycopy(paths,0,tmp,0,paths.length);
+			tmp[paths.length] = s;
+			field.set(null,tmp);
+			System.setProperty("java.library.path", System.getProperty("java.library.path") + File.pathSeparator + s);
+		} catch (IllegalAccessException e) {
+			throw new IOException("Failed to get permissions to set library path");
+		} catch (NoSuchFieldException e) {
+			throw new IOException("Failed to get field handle to set library path");
+		}
+	}
     
     public Runner(){
     	fourier = new FourierAnalysis(new DummyAudioInput(), 8192);
